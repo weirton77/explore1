fbcb3be0460c69ba8a47a492626209e596d45856
weirton77 <weirton77@users.noreply.github.com>
2015-01-09 14:07:44 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,8 +1,9 @@
-setwd("https://github.com/weirton77/explore1/blob/master
+"C:/Users/steve/Desktop/Exploratory/proj1"
 #original data prepared with dataprep.R
 power <- read.csv("power.csv", colClasses=c("character",rep("numeric",7)))
 power$postime=strptime(paste(power$date, " ", power$time),"%d/%m/%Y %H:%M:%S")
+par(mfrow = 1)
 #plot1
-#png(filename="plot1.png")   #turned off for demo
+#png(filename="plot1.png")
 hist(power$active, xlab ="Global Active Power (kilowatts)", main = "Global Active Power", col = "red", ylim=c(0,1200))
 #dev.off()
